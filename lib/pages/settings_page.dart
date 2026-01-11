869c1ed39f9b1647b7e5038aae444701cf55693f
donniebrasc <kalrizin@protonmail.com>
2026-01-10 21:02:40 -0500

    modified:   lib/pages/settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Settings and requirements page
 class SettingsPage extends StatefulWidget {
